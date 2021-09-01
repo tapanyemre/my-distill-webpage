a97515b268119e897e3c17972a03ab5553115708
Yunus Emre Tapan <tapan.y@northeastern.edu>
2021-09-01 20:46:04 +0300

    update

---
+++ Files/codes.R
@@ -0,0 +1,12 @@
+install.packages("distill")
+library(distill)
+
+create_website("website", title = "Yunus Emre Tapan")
+
+
+create_post("Building a {distill} academic webpage", slug= "buildingdistill")
+
+create_theme(name = 'lisa_theme')
+
+create_article("new article")
+
